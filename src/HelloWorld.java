3ea045fc2e742a3a7049d4d80ce23cfb9c4cb3ad
zijie.xiang@tcl.com <ZIJIE.XIANG@TCL.COM>
2020-10-09 10:30:11 +0800

    cpt 18

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,30 +1,100 @@
+import java.util.*;
 import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int rangeStart = 1;
-        int rangeEnd = 8;
-        int guessTotal = 5;
+        System.out.println("Input the row nums: ");
+        int Row = in.nextInt();
+        System.out.println("Input the col nums: ");
+        int Col = in.nextInt();
 
-        while (true) {
-            // generate random nums
-            int mod = rangeEnd - rangeStart;
-            if (rangeStart<0 || rangeEnd <0) {
-                System.out.println("Error: must start/end with pos int");
-            }
-            if (mod <= 1){
-                System.out.println("Wrong range: (" + rangeStart +", "+rangeEnd+")");
-            }
-            int bigRandom = (int) (Math.random()*rangeEnd*100);
-            int numberToGuess = (bigRandom%mod) + rangeStart;
-            if (numberToGuess <= rangeStart) {
-                numberToGuess = rangeStart + 1;
-            } else if (numberToGuess > rangeEnd){
-                numberToGuess = rangeEnd -1;
-            }
+        double[][] values = new double[Row][Col];
 
+        for (int i = 0; i<Row; i++){
+            for (int j = 0; j<Col; j++){
+                values[i][j] = Math.random()*10+5;
+            }
+        }
 
+        boolean cont = true;
+
+        while (cont){
+            System.out.println("Pls input the index u want: ");
+            System.out.println("1: The largest num in a row\n"+
+                    "2: The ave num in a row\n" +
+                    "3: All the largest nums\n" +
+                    "4: The largest num in a col\n");
+
+            int oprtId = in.nextInt();
+            int year = 0;
+            switch (oprtId){
+                case 1:
+                    System.out.println("Input the row index: ");
+                    year = in.nextInt() - 1;
+                    if (year < 0 || year > Row){
+                        System.out.println("wrong row index");
+                        break;
+                    }
+                    int maxval = 0;
+                    for (int i=0;i<values[year].length;i++){
+                        if (values[year][i] > values[year][maxval]){
+                            maxval = i;
+                        }
+                    }
+                    System.out.println("Row"+(year+1)+"\'s largest num is: "+values[year][maxval]);
+                    cont = false;
+                    break;
+                case 2:
+                    System.out.println("Input the row index: ");
+                    year = in.nextInt() - 1;
+                    if (year < 0 || year > Row){
+                        System.out.println("wrong row index");
+                        break;
+                    }
+                    double sums = 0;
+                    for (int i=0;i<values[year].length;i++){
+                        sums += values[year][i];
+                    }
+                    System.out.println("The average is: "+sums/values[year].length);
+                    cont = false;
+                    break;
+                case 3:
+                    //List<double> largests = new ArrayList<double>();
+                    for (int i=0;i<Row;i++){
+                        int Maxval = 0;
+                        for (int j=0; j<values[i].length;j++){
+                            if (values[i][j] > values[i][Maxval]){
+                                Maxval = i;
+                            }
+                        }
+                        //largests.add(values[i][Maxval]);
+                        System.out.println("The largest num of row"+i+"is: "+values[i][Maxval]);
+                    }
+                    cont = false;
+                    break;
+                case 4:
+                    System.out.println("Input the Column index: ");
+                    year = in.nextInt() - 1;
+                    if (year < 0 || year > Col){
+                        System.out.println("wrong col index");
+                        break;
+                    }
+                    int maxval2 = 0;
+                    for (int i=0;i<Row;i++){
+                        if (values[maxval2][year]<values[i][year]){
+                            maxval = i;
+                        }
+                    }
+                    System.out.println("The largest num in a col: "+values[maxval2][year]);
+                    cont = false;
+                    break;
+                default:
+                    System.out.println("Wrong index!\n"+
+                            "Program killed.");
+                    cont = false;
+                    break;
+            }
         }
     }
 }
---
+++ src/geekbang/MerchandiseV2DescAppMain.java
@@ -0,0 +1,19 @@
+package geekbang;
+import geekbang.supermarket.MerchandiseV2;
+
+public class MerchandiseV2DescAppMain {
+    public static void main(String[] args) {
+        MerchandiseV2 merchandise = new MerchandiseV2();
+
+//        merchandise.name = "书桌";
+//        merchandise.soldPrice = 999.9;
+//        merchandise.purchasePrice = 500;
+//        merchandise.count = 40;
+//        merchandise.id = "DESK9527";
+
+        // >> TODO 调用方法，完成对成员变量的操作
+        merchandise.init("书桌", "DESK9527", 40, 999.9, 500);
+
+        merchandise.describe();
+    }
+}
